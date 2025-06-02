4de8e1a3d32be908b949fcd093983f457d01bf28
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2025-06-02 13:48:30 -0400

    wip: adding cart state logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,35 @@ import NavBar from "./components/navbar";
 
 function App() {
   const [cart, setCart] = useState([])
+  //TODO: start with cart of every object, 0 quantity?
 
   //item = name, desc, img, price, quantity
 
-  const addToCart = (item) => {
-    setCart(cart => [...cart, item])
+  const updateCart = (item) => {
+    if (cart.some(obj => obj['name'] === item.name)){
+      updateQuantity(item)
+    } else {
+      addItem(item)
+    }
+
   }
+
+  const addItem = (item) => {
+    setCart( cart => [...cart, item])
+  }
+
+  const updateQuantity = (item) => {
+    newCart = cart.map((x) => {
+      if (x.name == item.name){
+        return {...x, quantity: item.quantity}
+      } else {
+        return {...x}
+      }
+    })
+
+    setCart([...newCart])
+  }
+
 
   const emptyCart = () => {
     setCart([])
